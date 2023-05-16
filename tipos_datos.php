c06f6553c8b5a4ccfa8ce955c4e05e352edefab4
DavidPineda02 <karertian2016@gmail.com>
2023-05-16 08:23:53 -0500

    feat: :sparkles: Tipos de datos PHP
    Complete el contenido en el archivo de Tipos de Datos

--- tipos_datos.php
+++ tipos_datos.php
@@ -8,10 +8,33 @@
     </head>
     <body>
         <?php
-        $price = 5000;
-        echo "that cost {$price}";
+            /* 1. String. Serie de caracteres */
+            $customer = "Falcao";
+            echo "The customer {$customer} bought <br>";
 
-        // Floating point number - floats
+            $favorite_food = "Panzerotti y Pizza";
+            echo "a {$favorite_food} <br>";
+
+            /* 2. Integer. Es un numero */
+            $price = 5000;
+            echo "that cost {$price}";
+
+            // 3. Floating point number - floats - doubles - reals numbers 
+            $goal_rate_per_game = 4.5;
+            echo "<br> and now he is celebrating his goal rate per futbol game of {$goal_rate_per_game}";
+
+            /* 4. Boolean. Tiene 2 valores (true) o (false) */
+            $state = true;
+            echo "<br> Is Falcao a champion? That is {$state}";
+
+            /* 5. Aceder Tipos de Datos  */
+            $type_data = gettype($price);
+            $type_datas = gettype($state);
+            $type_datass = gettype($goal_rate_per_game);
+
+            echo "<br> Tipo de dato es: {$type_data}";
+            echo "<br> Tipo de dato es: {$type_datas}";
+            echo "<br> Tipo de dato es: {$type_datass}"; 
         ?>
     </body>
 </html>
